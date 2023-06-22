41b89d492c1459c78e01d1105a25ec2894ae41b9
nashihu <ahmad.n.id@gmail.com>
2023-06-22 16:20:58 +0700

    add counter

--- src/lambda/payment.js
+++ src/lambda/payment.js
@@ -1,7 +1,7 @@
 import { google } from 'googleapis'
 import fetch from 'node-fetch'
 
-const { NEXT_PUBLIC_EMAIL, NEXT_PUBLIC_PPK } = process.env
+const { NEXT_PUBLIC_EMAIL, NEXT_PUBLIC_PPK, NEXT_PUBLIC_ISLOCAL } = process.env
 const scopes = [
   'https://www.googleapis.com/auth/datastore'
 ]
@@ -15,7 +15,7 @@ const jwtClient = new google.auth.JWT(
 )
 
 export function handler(event, context, callback) {
-  console.log(`apa ini ${NEXT_PUBLIC_EMAIL} ${NEXT_PUBLIC_PPK.replace(/\\n/g, '\n')}`)
+  print(`apa ini ${NEXT_PUBLIC_EMAIL} ${NEXT_PUBLIC_PPK.replace(/\\n/g, '\n')}`)
   const res = JSON.parse(event.body)
   jwtClient.authorize(async function (error, tokens) {
     if (error) {
@@ -24,15 +24,15 @@ export function handler(event, context, callback) {
         body: `unable generate token ${error}`
       })
     } else if (tokens.access_token === null) {
-      console.log('Provided service account does not have permission to generate access tokens')
+      print('Provided service account does not have permission to generate access tokens')
       callback(null, {
         statusCode: 401,
         body: 'error access token null'
       })
     } else {
       const accessToken = tokens.access_token
-      console.log("dapet ya?", accessToken)
-      console.log(`ini body nya ${res.custom_field3}`)
+      print("dapet ya?", accessToken)
+      print(`ini body nya ${res.custom_field3}`)
       if (res.transaction_status != "settlement") {
         callback(null, {
           statusCode: 201,
@@ -41,9 +41,11 @@ export function handler(event, context, callback) {
       }
       var body = {}
       var campaignId = ""
+      var amount = 0
       try {
         const cust = JSON.parse(res.custom_field3)
         campaignId = cust.campaignId
+        amount = cust.amount
         body = {
           "fields": {
             "name": {
@@ -69,26 +71,76 @@ export function handler(event, context, callback) {
           body: `error parse ${e}`
         })
       }
-      fetch(`https://firestore.googleapis.com/v1/projects/cipcipp-150996/databases/(default)/documents/${campaignId}?documentId=${res.transaction_id}`, {
-        body: JSON.stringify(body),
-        headers: {
-          'Authorization': `Bearer ${accessToken}`
-        },
-        method: 'POST'
-      })
-        .then(response => response.json())
-        .then(data => {
-          callback(null, {
-            statusCode: 200,
-            body: `${data}`
-          })
-        })
-        .catch((e) => {
-          callback(null, {
-            statusCode: 400,
-            body: `error ${e}`
-          })
-        })
+      const headers = {
+        'Authorization': `Bearer ${accessToken}`
+      }
+      const baseUrl = 'https://firestore.googleapis.com/v1/projects/cipcipp-150996/databases/(default)/documents'
+      var url = `${baseUrl}/${campaignId}?documentId=${res.transaction_id}`
+      addEntry(body, url, headers, callback)
+
+      url = `${baseUrl}/${campaignId}-sum/`
+      checkSum(url, headers, amount)
     }
   })
+}
+
+function checkSum(url, headers, addition) {
+  fetch(url, {
+    headers: headers,
+    method: 'GET'
+  }).then(response => response.json())
+    .then((miaw) => {
+      const currentSum = Number(miaw.documents[0].fields.sum.integerValue)
+      const nextSum = currentSum + addition
+      print(`next sum would be ${nextSum}`)
+      addSum(url, headers, nextSum)
+    })
+}
+
+function addSum(url, headers, nextSum) {
+  fetch(`${url}sum?updateMask.fieldPaths=sum`, {
+    headers: headers,
+    method: 'PATCH',
+    body: JSON.stringify({
+      "fields": {
+        "sum": {
+          "integerValue": `${nextSum}`
+        }
+      }
+    })
+  }).then(response => response.json())
+    .then((miaw) => {
+      print(`done here`)
+      // callback(null, {
+      //   statusCode: 200,
+      //   body: `${JSON.stringify(miaw)}`
+      // })
+    })
+}
+
+function addEntry(body, url, headers, callback) {
+  fetch(url, {
+    body: JSON.stringify(body),
+    headers: headers,
+    method: 'POST'
+  })
+    .then(response => response.json())
+    .then(data => {
+      callback(null, {
+        statusCode: 200,
+        body: `${JSON.stringify(data)}`
+      })
+    })
+    .catch((e) => {
+      callback(null, {
+        statusCode: 400,
+        body: `error ${e}`
+      })
+    })
+}
+
+function print(msg) {
+  if (NEXT_PUBLIC_ISLOCAL === "true") {
+    console.log(msg)
+  }
 }
